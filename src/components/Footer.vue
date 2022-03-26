9e3420038d5470bc6a976139e0a0ecbb406f7d37
Khetag <dzkhetag@gmail.com>
2022-03-26 20:25:51 +0300

    Add logo_ru_white.png.

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <div class="enigma_footer_area">
+      <div class="container">
+        <div class="col-md-12">
+          <p class="footer_info">© 2011 - 2022 ООО «ОЗБИО»</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
